849128db266c397a339c38d71ea36818387acf94
Kruszewski <58085942+Kruszewski@users.noreply.github.com>
2024-11-28 12:21:50 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # DoDomuDojade
+
+## Overview
+DoDomuDojade is an application that provides real-time tram departure information and meteorological data for Poznań. It fetches data from public APIs and displays it in a user-friendly interface.
+
+## Features
+- Real-time tram departure updates from the AWF73 stop.
+- Current meteorological data from the Poznań station.
+- User-friendly interface with customizable styles.
+
+## Installation
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/yourusername/DoDomuDojade.git
+   ```
+2. Navigate to the project directory:
+   ```bash
+   cd DoDomuDojade
+   ```
+3. Install the required dependencies:
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+## Usage
+Run the application using the following command:
+```bash
+python main.py
+```
--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-# Biblioteki, moduły, itp.
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QHBoxLayout
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QHBoxLayout, QTableWidget, QTableWidgetItem
 from PySide6.QtGui import QPixmap
 from PySide6.QtCore import QTimer, Qt
-import time, sys, requests, json
+import time
+import sys
+import requests
+import json
 from threading import Thread
-
-# Set up Selenium WebDriver
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-driver = webdriver.Chrome(options = options, service=Service(ChromeDriverManager().install()))         
 
 with open('data.json', 'r') as jsonfile:
     data = json.load(jsonfile)
-    awf73 = data["API"][0]["url"]
-    meteo = data["API"][1]["url"]
+    awf73 = data["API"][1]["url"]
+    meteo = data["API"][0]["url"]
     app_style = data["stylesheet"][0]["style"]
+
+print(awf73)
+print(meteo)
 
 class Tram_DataFetcher(Thread):
     def __init__(self, update_callback):
@@ -27,104 +29,93 @@ class Tram_DataFetcher(Thread):
         self.daemon = True
 
     def run(self):
-        # Set up Selenium WebDriver
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
         driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
 
         while True:
-            driver.get(awf73)
-            time.sleep(2)
-
-            # Zmienne z danymi
-            tramwaje = driver.find_elements(By.CLASS_NAME, "line")
-            odjazdy = driver.find_elements(By.CLASS_NAME, "time")
-            kierunki = driver.find_elements(By.CSS_SELECTOR, "div.direction")
-            # Listy
-            announcements_data = []
-            data = []
-            
             try:
-                informacje_a = driver.find_element(By.CLASS_NAME, "messages_in")
-                informacje = informacje_a.find_element(By.TAG_NAME, "a")
-                announcements_data.append(informacje.text)
+                driver.get(awf73)
+
+                # Czekanie na załadowanie elementów
+                wait = WebDriverWait(driver, 10)  # Czeka do 10 sekund
+                tramwaje = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "line")))
+                odjazdy = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "time")))
+                kierunki = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "direction")))
+                
+                announcements_data = []
+                tram_data = []
+
+                try:
+                    informacje_a = wait.until(EC.presence_of_element_located((By.ID, "messages")))
+                    informacje = informacje_a.find_element(By.TAG_NAME, "a")
+                    announcements_data.append(informacje.text)
+                except Exception as e:
+                    print("Nie znaleziono elementu z ogłoszeniami:", e)
+
+                for tramwaj, odjazd, kierunek in zip(tramwaje, odjazdy, kierunki):
+                    tram_data.append((tramwaj.text, odjazd.text, kierunek.text))
+
+                self.update_callback(tram_data, announcements_data)
+
             except Exception as e:
-                print("Nie znaleziono elementu. Przechodzę do następnej czynności:", e)
+                print("Błąd przy pobieraniu danych:", e)
 
-            for tramwaj, odjazd, kierunek in zip(tramwaje, odjazdy, kierunki):
-                data.append(f"Tramwaj: {tramwaj.text}, Odjazd: {odjazd.text}, Kierunek: {kierunek.text}")
-
-            # Update the UI
-            self.update_callback(data, announcements_data)
             time.sleep(1)
 
         driver.quit()
 
 class Meteo_DataFetcher(Thread):
-    def __init__(self):
+    def __init__(self, update_callback):
         super().__init__()
-    
-    def request(self):
-        # The API endpoint
-        url = "meteo"
+        self.update_callback = update_callback
+        self.daemon = True
 
-        # A GET request to the API
-        response = requests.get(meteo)
+    def run(self):
+        while True:
+            try:
+                response = requests.get(meteo)
+                if response.status_code == 200:
+                    data = response.json()
+                    self.update_callback(data)
+                else:
+                    print("Błąd przy pobieraniu danych pogodowych:", response.status_code)
+            except Exception as e:
+                print("Błąd przy połączeniu z API pogodowym:", e)
 
-        # Print the response
-        print(response.text)
+            time.sleep(60)  # Odświeżanie co minutę
 
 class Footer(QWidget):
     def __init__(self):
         super().__init__()
-        
-        # Tworzymy layout na stopkę
         layout = QHBoxLayout()
-        
-        # Możesz dodać logo
         logo = QLabel()
-        pixmap = QPixmap("logo.png")  # Wstaw ścieżkę do swojego logo
-        logo.setPixmap(pixmap.scaled(100, 50))  # Skaluje logo do odpowiedniego rozmiaru
-        
-        # Dodajemy logo do layoutu
+        pixmap = QPixmap("logo.png")
+        logo.setPixmap(pixmap.scaled(100, 50))
         layout.addWidget(logo)
-        
-        # Możesz dodać inne elementy do stopki, jeśli chcesz
         footer_text = QLabel("© 2024 DoDomuDojadę.")
         layout.addWidget(footer_text)
-
-        # Ustawiamy layout
         self.setLayout(layout)
 
 class Loading(QWidget):
     def __init__(self):
         super().__init__()
-
-        # Tworzenie etykiety dla kropek
         self.loading_label = QLabel("Ładowanie danych", self)
         self.dot_label = QLabel("", self)
         self.loading_label.setAlignment(Qt.AlignCenter)
         self.dot_label.setAlignment(Qt.AlignCenter)
-
-        # Ustawienia layoutu
         layout = QVBoxLayout()
         layout.addWidget(self.loading_label)
         layout.addWidget(self.dot_label)
         self.setLayout(layout)
-
-        # Inicjalizacja zmiennych do animacji
         self.dot_count = 0
         self.dots = ["", ".", "..", "..."]
-
         self.dot_label.setStyleSheet("font-size: 48px;")
-
-        # Ustawienie timera do animacji
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_dots)
-        self.timer.start(500)  # Czas odświeżania w milisekundach
+        self.timer.start(500)
 
     def update_dots(self):
-        # Aktualizacja kropek w etykiecie
         self.dot_label.setText(f"{self.dots[self.dot_count]}")
         self.dot_count = (self.dot_count + 1) % len(self.dots)
 
@@ -135,41 +126,53 @@ class MainWindow(QWidget):
         self.setFixedSize(500, 600)
         self.setStyleSheet(app_style)
         self.layout = QVBoxLayout()
-
-        # Inicjalizacja widgetów QLabel dla tramwajów i ogłoszeń
-        self.tram_list = QLabel()
+        self.tram_table = QTableWidget()
         self.announcements_list = QLabel()
         self.footer = Footer()
         self.loading_widget = Loading()
-        # Zezwolenie na zawiajnie tekstu
-        self.announcements_list.setWordWrap(True)
+        
+        self.tram_table.setColumnCount(3)
+        self.tram_table.horizontalHeader().setVisible(False)
+        self.tram_table.verticalHeader().setVisible(False)
 
-        # Dodanie widgetów do układu
+        
         self.layout.addWidget(self.loading_widget)
         self.layout.addWidget(self.announcements_list)
-        self.layout.addWidget(self.tram_list)
+        self.layout.addWidget(self.tram_table)
         self.layout.addWidget(self.footer)
-        
-        self.tram_list.hide()
+        self.tram_table.hide()
         self.announcements_list.hide()
         self.footer.hide()
-
         self.setLayout(self.layout)
-
-        # Startowanie wątku pobierającego dane
-        self.data_fetcher = Tram_DataFetcher(self.update_label)
+        self.data_fetcher = Tram_DataFetcher(self.update_table)
         self.data_fetcher.start()
 
-    def update_label(self, data, announcements_data):
-        # Po zakończeniu pobierania danych ukryj animację i wyświetl interfejs
+    def update_table(self, tram_data, announcements_data):
         self.loading_widget.hide()
-        self.tram_list.show()
-        self.announcements_list.show()
+        self.tram_table.show()
         self.footer.show()
 
-        # Update the label with the fetched data
-        self.tram_list.setText("\n".join(data))
-        self.announcements_list.setText("\n".join(announcements_data))
+        # Sprawdzenie, czy tabela ma już jakieś dane
+        current_row_count = self.tram_table.rowCount()
+
+        # Zaktualizuj lub dodaj wiersze
+        for row, (tramwaj, odjazd, kierunek) in enumerate(tram_data):
+            if row < current_row_count:
+                self.tram_table.item(row, 0).setText(tramwaj)
+                self.tram_table.item(row, 1).setText(odjazd)
+                self.tram_table.item(row, 2).setText(kierunek)
+            else:
+                # Dodaj nowy wiersz, jeśli nie ma wystarczającej ilości wierszy
+                self.tram_table.insertRow(row)
+                self.tram_table.setItem(row, 0, QTableWidgetItem(tramwaj))
+                self.tram_table.setItem(row, 1, QTableWidgetItem(odjazd))
+                self.tram_table.setItem(row, 2, QTableWidgetItem(kierunek))
+
+        if announcements_data:
+            self.announcements_list.setText("\n".join(announcements_data))
+        else:
+            self.announcements_list.setText("Brak dostępnych ogłoszeń.")
+        self.announcements_list.show()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
