2c19d9ad6f289e5ded09abbdb16e7208f696fbc8
Kruszewski <58085942+Kruszewski@users.noreply.github.com>
2024-12-19 12:21:29 +0100

    huh

---
+++ footer.php
@@ -0,0 +1,7 @@
+<div style="margin-top: 100px;"></div>
+
+<div id="footer">
+    <div class="inner">
+        Developed with <span style="color: red;"><i class="bi bi-suit-heart-fill"></i></span> by <span style="color: #5865F2;">Loïc</span> <i>(aka <span style="color: #5865F2;">Cut0x</span>)</i> !
+    </div>
+</div>
