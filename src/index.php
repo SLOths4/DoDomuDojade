b88540b83285c22a70ac8405969ed2a468c9ba1e
Kruszewski <58085942+Kruszewski@users.noreply.github.com>
2025-01-14 13:17:49 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>DoDomuDojadę</title>
-    <link rel="icon" type="image/x-icon" href="/resources/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="resources/favicon.ico">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
@@ -26,7 +26,6 @@
         // Fetching data form API
         $weather_data = get($weather_url);
         // Variables
-        $id_stacji = $weather_data["id_stacji"];
         $data_pomiaru = $weather_data["data_pomiaru"];
         $godzina_pomiaru = $weather_data["godzina_pomiaru"];
         $kierunek_wiatru = $weather_data["kierunek_wiatru"];
