43517fe84800eed6fcd75e7a2f9243198cf0677f
Kruszewski <58085942+Kruszewski@users.noreply.github.com>
2025-01-15 09:49:55 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -15,29 +15,9 @@
 
     <!-- IMPORT WEATHER MODULE -->
     <div id="weather">
-    <?php
-    // Import get utility
-    include('./utilities/ApiClient.php');
-
-
-
-    ?>
     </div>
 
     <div id="schedule">
-    <?php
-    include("./utilities/CalendarService.php");
-    $calendar = new Calendar();
-    $events = $calendar->get_events();
-
-    // Display the events
-    foreach ($events as $event) {
-        echo "Title: " . htmlspecialchars($event['summary']) . "<br>";
-        echo "Start: " . htmlspecialchars($event['start']) . "<br>";
-        echo "End: " . htmlspecialchars($event['end']) . "<br>";
-        echo "Description: " . htmlspecialchars($event['description']) . "<br><br>";
-    }
-    ?>
     </div>
 
 
